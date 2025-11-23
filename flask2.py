1341bc73b76c0bc68a4c48e8d25e0f26604ea09e
Bartosz Bryniarski <brylka@jelenia.com>
2025-11-23 10:10:39 +0100

    Niedziela

--- flask2.py
+++ flask2.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
+# Pobranie klucza OpenAI
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY_2')
+
+# Inicjalizacja klienta OpenAI
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+# Model do użycia
+model = "gpt-3.5-turbo"
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return render_template('flask2.html')
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    user_prompt = request.form.get('question')
+
+    response = client.chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": user_prompt}],
+        temperature=0.7
+    )
+
+    answer = response.choices[0].message.content
+
+    return answer
+
+
 
 
 if __name__ == '__main__':
