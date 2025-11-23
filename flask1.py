f2f3b52b0fdc3d4e81205623c7968dfe948cf9ed
Bartosz Bryniarski <brylka@jelenia.com>
2025-11-23 09:09:17 +0100

    Niedziela

---
+++ flask1.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('flask1.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
